34831c9b1a64306f32f8d019ceabbaf60e18583b
SANTHOSHKUMAR-CBE <santhoshkumarsenthilvel@gmail.com>
2025-05-13 18:58:17 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Hero from './Hero';
+
+function App() {
+  return (
+    <div>
+      <Hero />
+      {/* Other components */}
+    </div>
+  );
+}
+
+export default App;
