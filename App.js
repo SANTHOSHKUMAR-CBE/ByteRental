180bfa769bbe7be0bac80f5f7368a831c50f894f
SANTHOSHKUMAR-CBE <santhoshkumarsenthilvel@gmail.com>
2025-05-24 17:44:41 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import Hero from './Hero';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Hero from "./Hero";
+import UserLogin from "./UserLogin";
+import Signup from "./Signup";
+import AdminLogin from "./AdminLogin";
 
-function App() {
-  return (
-    <div>
-      <Hero />
-      {/* Other components */}
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Hero />} />
+                <Route path="/userlogin" element={<UserLogin />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/adminlogin" element={<AdminLogin />} />
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
